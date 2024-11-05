17bf490266040d79894a00df377d8f96303a6830
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-05 23:06:56 +0530

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-module "expense-terraform" {
-    source = "./modules/vpc"
-    vpc_cidr_block = var.vpc_cidr_block
-    tags = var.tags
-    env = var.env  
+module "expense_terraform" {
+  source            = "./modules/vpc"
+    vpc_cidr_block  = var.vpc_cidr_block
+    tags            = var.tags
+    env             = var.env
+    public_subnets  = var.public_subnets
+    web_subnets     = var.web_subnets
+    app_subets      = var.app_subnets
+    db_subnets      = var.db_subnets
+    azs             = var.azs
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,5 +1,38 @@
 resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr_block
     tags = merge(var.tags, {Name = var.env})
-  
 }
+
+resource "aws_subnet" "public" {
+    count = length(var.public_subnets)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.public_subnets[count.index]
+    tags = merge(var.tags, {Name = "public-subnets"})
+    availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "web" {
+    count = length(var.public_subnets)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.web_subnets[count.index]
+    tags = merge(var.tags, {Name = "web-subnets"})
+    availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "app" {
+    count = length(var.app_subnets)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.app_subnets[count.index]
+    tags = merge(var.tags, {Name = "app-subnets"})
+    availability_zone = var.azs[count.index]
+}
+
+resource "aws_subnet" "db" {
+    count = length(var.db_subnets)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.db_subnets[count.index]
+    tags = merge(var.tags, {Name = "db-subnets"})
+    availability_zone = var.azs[count.index]
+}
+
+
