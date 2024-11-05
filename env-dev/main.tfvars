b98cc12a7c120ac3cf8a059e4c08f10e0659106b
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-05 22:10:55 +0530

    test

---
+++ env-dev/main.tfvars
@@ -0,0 +1,7 @@
+vpc_cidr_block = "10.10.0.0/16"
+env = "dev"
+tags = {
+    company = "NVK"
+    bu_unit = "Birds"
+    project_name = "expense"
+}
--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 module "expense-terraform" {
     source = "./modules/vpc"
     vpc_cidr_block = var.vpc_cidr_block
+    tags = var.tags
+    env = var.env  
 }
