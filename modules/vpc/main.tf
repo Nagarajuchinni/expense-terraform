154d9f5625552a914112ffa1030b6443822c502f
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-06 09:03:13 +0530

    test

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -147,7 +147,7 @@ resource "aws_vpc_peering_connection" "peer" {
 
 resource "aws_route" "default-vpc-peer-route" {
   route_table_id            = var.default_route_table_id
-  destination_cidr_block    = var.default_vpc_id
+  destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
 
